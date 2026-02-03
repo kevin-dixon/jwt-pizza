c87b2fdbf09bc57fffe252120fc1ed223e7612e1
kevin-dixon <87494457+kevin-dixon@users.noreply.github.com>
2026-02-03 16:41:24 -0700

    ci pipeline and badge

--- tests/auth.spec.ts
+++ tests/auth.spec.ts
@@ -1,15 +1,45 @@
 import { test, expect } from 'playwright-test-coverage';
 
 test('test', async ({ page }) => {
-  await page.goto('http://localhost:5174/');await page.goto('http://localhost:5174/');
-  await page.getByRole('link', { name: 'Register' }).click();
-  await page.getByRole('textbox', { name: 'Full name' }).click();
-  await page.getByRole('textbox', { name: 'Full name' }).fill('exampleuser');
-  await page.getByRole('textbox', { name: 'Email address' }).click();
-  await page.getByRole('textbox', { name: 'Email address' }).fill('example@email.com');
-  await page.getByRole('textbox', { name: 'Password' }).click();
-  await page.getByRole('textbox', { name: 'Password' }).fill('testpassword');
-  await page.getByRole('button', { name: 'Register' }).click();
+    await page.goto('http://localhost:5174/'); await page.goto('http://localhost:5174/');
+    await page.getByRole('link', { name: 'Register' }).click();
+    await page.getByRole('textbox', { name: 'Full name' }).click();
+    await page.getByRole('textbox', { name: 'Full name' }).fill('exampleuser');
+    await page.getByRole('textbox', { name: 'Email address' }).click();
+    await page.getByRole('textbox', { name: 'Email address' }).fill('example@email.com');
+    await page.getByRole('textbox', { name: 'Password' }).click();
+    await page.getByRole('textbox', { name: 'Password' }).fill('testpassword');
+    await page.getByRole('button', { name: 'Register' }).click();
 
-  //add verification for successful registration
+    //add verification for successful registration
 });
+
+test('login test', async ({ page }) => {
+    await page.route('*/**/api/auth', async (route) => {
+        const loginReq = { email: 'd@jwt.com', password: 'a' };
+        const loginRes = {
+            user: {
+                id: 3,
+                name: 'Kai Chen',
+                email: 'd@jwt.com',
+                roles: [{ role: 'diner' }],
+            },
+            token: 'abcdef',
+        };
+        expect(route.request().method()).toBe('PUT');
+        expect(route.request().postDataJSON()).toMatchObject(loginReq);
+        await route.fulfill({ json: loginRes });
+    });
+
+    /*
+    await page.getByRole('link', { name: 'Login', exact: true }).click();
+    await page.getByRole('textbox', { name: 'Email address' }).fill('d@jwt.com');
+    await page.getByRole('textbox', { name: 'Email address' }).press('Tab');
+    await page.getByRole('textbox', { name: 'Password' }).fill('a');
+    await page.getByRole('button', { name: 'Login' }).click();
+    */
+    
+    //add verification for successful login
+
+});
+
