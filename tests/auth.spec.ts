8e6433fc95b5c2b9641c05cab3649fc907bab5a9
kevin-dixon <87494457+kevin-dixon@users.noreply.github.com>
2026-02-03 16:24:39 -0700

    initial testing

--- tests/auth.spec.ts
+++ tests/auth.spec.ts
@@ -1,5 +1,15 @@
 import { test, expect } from 'playwright-test-coverage';
 
 test('test', async ({ page }) => {
-  await page.goto('http://localhost:5174/');
+  await page.goto('http://localhost:5174/');await page.goto('http://localhost:5174/');
+  await page.getByRole('link', { name: 'Register' }).click();
+  await page.getByRole('textbox', { name: 'Full name' }).click();
+  await page.getByRole('textbox', { name: 'Full name' }).fill('exampleuser');
+  await page.getByRole('textbox', { name: 'Email address' }).click();
+  await page.getByRole('textbox', { name: 'Email address' }).fill('example@email.com');
+  await page.getByRole('textbox', { name: 'Password' }).click();
+  await page.getByRole('textbox', { name: 'Password' }).fill('testpassword');
+  await page.getByRole('button', { name: 'Register' }).click();
+
+  //add verification for successful registration
 });
